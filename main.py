6d6d4ef5aa00ef1d8b201bd6999de6fe9baa4061
mackaronina <116784694+mackaronina@users.noreply.github.com>
2023-06-26 12:55:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from flask import Flask
 api_id = 17453825
 api_hash = 'aa6df76596b13eb999078e2e9796ff95'
 
-client = TelegramClient('session_name',api_id,api_hash)
-client.start()
 
 app = Flask(__name__)
-client.send_message(-1001694727085, 'Server started')
 @app.route('/')
 def get_ok():
+    client = TelegramClient('session_name',api_id,api_hash)
+    client.start()
     m = client.send_message(-1001694727085, 'Hello! Talking to you from Telethon')
     time.sleep(2)
     client.delete_messages(entity = -1001694727085, message_ids = [m.id])
