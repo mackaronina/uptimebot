4834380032206e51644eb4df05cd7746c0e3d1ea
mackaronina <116784694+mackaronina@users.noreply.github.com>
2023-06-26 13:18:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ api_hash = 'aa6df76596b13eb999078e2e9796ff95'
 app = Flask(__name__)
 @app.route('/')
 def get_ok():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     with TelegramClient('session_name', api_id, api_hash, loop=loop) as client:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
         #client = TelegramClient('session_name', api_id, api_hash, loop=loop)
         client.start()
         m = client.send_message(-1001694727085, 'Hello! Talking to you from Telethon')
