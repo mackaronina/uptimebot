5d580cc38e96ec0d427105463449fce34bf2c9fc
mackaronina <116784694+mackaronina@users.noreply.github.com>
2024-03-16 15:37:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -52,7 +52,8 @@ def get_morning():
     asyncio.set_event_loop(loop)
     with TelegramClient(StringSession(ses), api_id, api_hash, loop=loop) as client:
         client.start()
-        m = client.send_message('@silero_voice_bot', generate_text())
+        text = generate_text()
+        m = client.send_message('@silero_voice_bot', text)
         time.sleep(5)
         m = client.get_messages('@silero_voice_bot', ids=m.id+1)
         client.forward_messages('@NekocringeBot', m)
